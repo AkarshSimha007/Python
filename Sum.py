b0872784a8bad961e3c6cc3a5eba90c00a6df856
AKARSH.H.SIMHA <akarsh07simha@gmail.com>
2020-09-30 20:10:39 +0530

    Added A web page parser to compute the sum of numbers occuring in a webpage using beautifulsoup and urllib

---
+++ Sum.py
@@ -0,0 +1,18 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+sum=0
+tagfinder = soup.find_all("span")
+for line in tagfinder:
+    sum+=int(line.text)
+print(sum)
